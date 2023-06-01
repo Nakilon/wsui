b10d40a9496adc735bcb617b22bc936b62795837
Victor Maslov <nakilon@gmail.com>
2023-06-01 17:11:02 +0300

    working example plus-minus

--- examples/plus-minus.rb
+++ examples/plus-minus.rb
@@ -1,7 +1,10 @@
 require "../lib/wsui"
 
 x = 0
-WSUI.start 7001, "" do
-  x = rand
-  x
+WSUI.start 7001, "" do |m|
+  [[nil, [
+    WSUI::S.new("^", ->_{ x += 1 }),
+    x,
+    WSUI::S.new("v", ->_{ x -= 1 }),
+  ], nil]]
 end
--- lib/wsui.rb
+++ lib/wsui.rb
@@ -3,16 +3,31 @@ module WSUI
   require "async/websocket/adapters/rack"
   require "async/http/endpoint"
   require "falcon"
+  S = Struct.new :value, :f do
+    def click
+      f.call self
+    end
+  end
   def self.start port, html, &b
     app = Module.new do
       @connections = Set.new
       @port = port
       @b = b
       def self.call env
+        f = lambda do |o|
+          if o.is_a? S
+            {wsui_id: o.__id__, value: o.value}
+          elsif o.respond_to? :each
+            o.map &f
+          else
+            o
+          end
+        end
         Async::WebSocket::Adapters::Rack.open env, protocols: %w{ ws } do |connection|
           @connections << connection
           while message = connection.read
-            t = @b.call(message).to_json
+            ObjectSpace._id2ref(message[:id]).click if message.key? :id
+            t = f.call(@b.call(message)).to_json
             @connections.each do |connection|
               connection.write t
               connection.flush
@@ -27,21 +42,58 @@ module WSUI
               <script src="https://github.com/processing/p5.js/releases/download/v1.4.2/p5.min.js"></script>
               <script src="https://cdn.jsdelivr.net/gh/abachman/p5.websocket/dist/p5.websocket.min.js"></script>
               <script>
-                var data;
+                var all = null;
+                var regions = [];
                 function setup() {
-                  frameRate(10);
+                  frameRate(2);
                   createCanvas(windowWidth, windowHeight);
+                  textAlign(LEFT, CENTER);
                   connectWebsocket("ws://localhost:#{@port}");
-                }
-                function messageReceived(input) {
-                  data = input;
-                }
+                };
+                function messageReceived(data) {
+                  all = JSON.parse(data);
+                };
                 function draw() {
-                  console.log(data);
+                  console.log(all);
+                  clear();
+                  regions = [];
+                  let fv = function(o, left, top, width, height) {
+                    if (o === null) return;
+                    if (o.wsui_id) {
+                      regions.push({id: o.wsui_id, left: left, top: top, width: width, height: height});
+                      o = o.value;
+                    };
+                    if (Number.isFinite(o) || typeof o === "string" || o instanceof String) {
+                      textSize(min(100, textSize() * width / textWidth(o)));
+                      text(o, left, top + height / 2)
+                    } else o.forEach( function(e, i) {
+                      fh(e, left, top + height / o.length * i, width, height / o.length);
+                    } );
+                  };
+                  let fh = function(o, left, top, width, height) {
+                    if (o === null) return;
+                    if (o.wsui_id) {
+                      regions.push({id: o.wsui_id, left: left, top: top, width: width, height: height});
+                      o = o.value;
+                    };
+                    if (Number.isFinite(o) || typeof o === "string" || o instanceof String) {
+                      textSize(min(100, textSize() * width / textWidth(o)));
+                      text(o, left, top + height / 2)
+                    } else o.forEach( function(e, i) {
+                      fv(e, left + width / o.length * i, top, width / o.length, height);
+                    } );
+                  };
+                  fv(all, 0, 0, windowWidth, windowHeight);
                   sendMessage({});
-                }
-                function mousePressed() {
-                }
+                };
+                function mouseClicked() {
+                  regions.some( function(e) {
+                    if (mouseX >= e.left && mouseX <= e.left + e.width && mouseY >= e.top && mouseY <= e.top + e.height) {
+                      sendMessage({id: e.id});
+                      return true;
+                    };
+                  } );
+                };
               </script>
             </head>
             <body style="margin:0"><main></main></body>
